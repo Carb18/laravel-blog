284771d7b3050579261b2e3950c1b76d9740ffb1
Camilo Ruiz <caruizb@ut.edu.co>
2024-07-12 13:57:51 -0500

    Creando registros de prueba

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -16,8 +16,19 @@ class PostFactory extends Factory
      */
     public function definition(): array
     {
+        // CONTROL DE PUBLISHED
+        $published = $this->faker->randomElement([true, false]);
+        $published_at = $published ? $this->faker->dateTimeBetween('-1 years', 'now') : null;
         return [
-            //
+            'title' => $this->faker->sentence(),
+            'slug' => $this->faker->slug(),
+            'excerpt' => $this->faker->text(200),
+            'body' => $this->faker->paragraph(),
+            'image_path' => $this->faker->imageUrl(1280, 720),
+            'published' => $published,
+            'category_id' => rand(1, 20),
+            'user_id' => rand(1, 20),
+            'published_at' => $published_at,
         ];
     }
 }
